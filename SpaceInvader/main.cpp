c72efb7f19d7cf7aa21500c30d8011ff44367f6c
Vaidas <vaidas@MacBook-Pro.local>
2017-01-11 15:31:21 +0000

    adding

--- SpaceInvader/main.cpp
+++ SpaceInvader/main.cpp
@@ -8,46 +8,56 @@
 
 int rightMoves=0;
 bool rightSide=true, leftSide=false;
-BulletCl bullet;
+BulletCl bullet[10];
 float bulletY;
 bool createBullet=true;
+InvadersCl invaders[55];
+sf::Vector2f playerPosition;
+int j=0;
+int invaderShoot=0;
+int bulletNum=0;
+int invaderNum=-1;
+int shootedInvaders[55];
+int shootedListCounter=0;
+int leftSideScreen=0;
+int rightSideScreen=220;
+bool touchPlayer =false;
+sf::Vector2f mostRight;
+int shooted=0, shootedCollumn=0;
+
 int main()
 {
-
     //create Window
     sf::RenderWindow window(sf::VideoMode(600, 400), "Invader Game");
     // setup any shapes or variables.
     sf::RectangleShape player(sf::Vector2f(40, 20));
-    // my code
-    //sf::RectangleShape invader(sf::Vector2f(20, 20));
-
+// my code*************************************************************************
     // Invaders positions
     float invaderX = 0;
     float invaderY = 0;
     float invaderSpeed = 1.0f;
     float bulletSpeed=0.1f;
 // array of invaders
-    InvadersCl invaders[55];
-    int invaderNum=0;
-    for (int i=0;i<55;i++){
+
+    for (int j=0;j<55;j++){
         //moving line when is 11 invaders
-        if (invaderNum!=0 && invaderNum!=1 && invaderNum%11==0){
+        if (j!=0 && j!=1 && j%11==0){
             invaderY+=25;
             invaderX=0;
         }
-        invaders[invaderNum]=InvadersCl();
-        invaders[invaderNum].invader.setPosition(sf::Vector2f(invaderX,invaderY));
+        //std:: cout<<j;
+        invaders[j]=InvadersCl();
+        invaders[j].invader.setPosition(sf::Vector2f(invaderX,invaderY));
         invaderX+=35;
         invaderNum++;
     }
-
-
     float playerX = 1;
     float playerY = 379;
     float playerSpeed = 1.0f;
-
     player.setFillColor(sf::Color::Red);
     player.setPosition(playerX, playerY);
+// ***********************************************************************************************
+
     // set timepeFrame to 1 60th of a second. 60 frames per second
     sf::Time timePerFrame = sf::seconds(1.0f / 60.0f);
     sf::Time timeSinceLastUpdate = sf::Time::Zero;
@@ -55,101 +65,142 @@ int main()
     sf::Clock clock;
     clock.restart();
     invaderY = 0;
-    while (window.isOpen())
-    {
-
-        invaderNum=0;
-        invaderX=0;
+    while (window.isOpen()) {
+        invaderNum = 0;
+        invaderX = 0;
         // check if the close window button is clicked on.
         sf::Event event;
-        while (window.pollEvent(event))
-        {
+        while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed)
                 window.close();
         }
-        //get the time since last update and restart the clock
-        timeSinceLastUpdate += clock.restart();
-        //update every 60th of a second
-        //only when the time since last update is greate than 1/60 update the world.
-        if (timeSinceLastUpdate > timePerFrame) {
-            // get keyboard input.
-            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
-                playerX = playerX - playerSpeed;
-            }
-            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
-                playerX = playerX + playerSpeed;
-            }
-//Bullets ******************************************
-           for (int i=0;i<380;i++){
-                if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space)) {
-                    sf::Vector2f playerPosition = player.getPosition();
-                    bulletY= bulletY-bulletSpeed;
-                    bullet.bullet.setPosition(sf::Vector2f(playerPosition.x+15, playerPosition.y-10));
-                    bullet.bullet.move(sf::Vector2f(0, bulletY));
-                }
-            }
-//my code for moving invaders************************************************
-        if(rightSide) {
-            invaderX = invaderX + invaderSpeed;
-            for (int i = 0; i < 55; i++) {
-                invaders[invaderNum].invader.move(invaderX, invaderY);
-                invaderNum++;
-            }
-            rightMoves++;
-            if (rightMoves == 220) {
-                rightSide = false;
-                leftSide = true;
-                invaderNum = 0;
-                for (int i = 0; i < 55; i++) {
-                    sf::Vector2f position =invaders[i].invader.getPosition();
-                    invaders[i].invader.setPosition(sf::Vector2f(position.x,position.y+10));
-                }
+// for checking is it touch player
+        for (int i = 0; i < 55; i++) {
+            if (invaders[i].invader.getGlobalBounds().intersects(player.getGlobalBounds()) == true) {
+                touchPlayer = true;
             }
         }
-            if(leftSide){
-                invaderX= invaderX-invaderSpeed;
-                for (int i = 0; i < 55; i++) {
-                    invaders[invaderNum].invader.move(invaderX, invaderY);
-                    invaderNum++;
+        // until shoot all invaders
+        if (shootedListCounter != 55 && touchPlayer == false) {
+            //get the time since last update and restart the clock
+            timeSinceLastUpdate += clock.restart();
+            //update every 60th of a second
+            //only when the time since last update is greate than 1/60 update the world.
+            if (timeSinceLastUpdate > timePerFrame) {
+                // get keyboard input.
+                if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
+                    playerX = playerX - playerSpeed;
+                    playerPosition = player.getPosition();
                 }
-                rightMoves--;
-                if(rightMoves==0){
-                    rightSide=true;
-                    leftSide=false;
-                    for (int i = 0; i < 55; i++) {
-                        sf::Vector2f position =invaders[i].invader.getPosition();
-                        invaders[i].invader.setPosition(sf::Vector2f(position.x,position.y+10));
+                if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
+                    playerX = playerX + playerSpeed;
+                    playerPosition = player.getPosition();
+                }
+//for bullets and enamies
+                for (int i = 0; i < 55; i++) {
+                    if (invaders[i].invader.getGlobalBounds().intersects(bullet[bulletNum].bullet.getGlobalBounds()) ==
+                        true) {
+                        shootedInvaders[shootedListCounter] = i;
+                        invaders[i].invader.setPosition(sf::Vector2f(-350, -350));
+                        shootedListCounter++;
+                        bullet[bulletNum].bullet.setPosition(sf::Vector2f(playerPosition.x + 15, -5));
+                        invaderShoot++;
+
                     }
                 }
-            }
+//my code for moving invaders************************************************
+                if (rightSide) {
+                    invaderX = invaderX + 1;
+                    for (int i = 0; i < 55; i++) {
+                        invaders[i].invader.move(invaderX, invaderY);
+
+                    }
+                    rightMoves++;
+                    if (rightMoves == rightSideScreen) {
+                        rightSide = false;
+                        leftSide = true;
+
+                        // invaderNum = 0;
+                        for (int j = 0; j < 55; j++) {
+                            sf::Vector2f position = invaders[j].invader.getPosition();
+                            invaders[j].invader.setPosition(sf::Vector2f(position.x, position.y + 10));
+                        }
+                    }
+                }
+                if (leftSide) {
+                    invaderX = invaderX - 1;
+                    for (int i = 0; i < 55; i++) {
+                        invaders[i].invader.move(invaderX, invaderY);
+
+                    }
+                    rightMoves--;
+                    if (rightMoves == leftSideScreen) {
+                        rightSide = true;
+                        leftSide = false;
+                        for (int i = 0; i < 55; i++) {
+                            sf::Vector2f position = invaders[i].invader.getPosition();
+                            invaders[i].invader.setPosition(sf::Vector2f(position.x, position.y + 10));
+                        }
+                    }
+                }
 //**********end of moving invaders ******************************************************
+                // update any variables..
+                player.setPosition(playerX, playerY);
+                // clear the screen and draw all the shapes
+                window.clear();
+                window.draw(player);
 
-            // update any variables..
-            player.setPosition(playerX, playerY);
+// starts bullets**********************************************************************************
+                //bullet[bulletNum].bullet.setPosition(sf::Vector2f(playerPosition.x + 15, 377));
+                window.draw(bullet[bulletNum].bullet);
+                if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space)) {
+                    if (createBullet) {
+                        bullet[bulletNum].bullet.setPosition(sf::Vector2f(playerPosition.x + 15, 377));
+                        window.draw(bullet[bulletNum].bullet);
+                        createBullet = false;
+                    }
+                }
+                bullet[bulletNum].bullet.move(sf::Vector2f(0, -20));
+                sf::Vector2f bulletPosition = bullet[bulletNum].bullet.getPosition();
+// getting bullet position and checking is it reach top op screen
+                if (bulletPosition.y <= 0.0f) {
+                    createBullet = true;
+                }
+                //Drawing invaders ******************************
+                for (int k = 0; k < 55; k++) {
+                    if (k != shootedInvaders[shootedListCounter]) {
+                        window.draw(invaders[k].invader);
+                    }
+                }
 
-            // clear the screen and draw all the shapes
-            window.clear();
-            window.draw(player);
-            for (int i=0;i<55;i++){
-                window.draw(invaders[i].invader);
+//****Ends of bullets*************************************************************************
+
+                window.display();
+                // reset the timeSinceLastUpdate to 0
+                timeSinceLastUpdate = sf::Time::Zero;
             }
-
-            window.draw(bullet.bullet);
-
+            //my code
+            if (playerX == 0) {
+                playerX = 559;
+            } else if (playerX == 560) {
+                playerX = 1;
+            }
+        }
+// if touch the player  red screen
+        else if (touchPlayer) {
+            window.clear(sf::Color::Red);
             window.display();
-            // reset the timeSinceLastUpdate to 0
-            timeSinceLastUpdate = sf::Time::Zero;
         }
-        //my code
-        if (playerX==0){
-            playerX=559;
-        }
-        else if(playerX==560){
-            playerX=1;
+
+// if all invaders shooted
+        else {
+            window.clear(sf::Color::Green);
+            window.display();
+
+
         }
 
     }
-
 
     return 0;
 }
